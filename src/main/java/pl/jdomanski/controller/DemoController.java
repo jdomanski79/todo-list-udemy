9c53b57f117acb906491b373d8aeeab31a010e74
jd <jd@jd.pl>
2018-07-04 13:01:20 +0000

    before simple service challenge

---
+++ src/main/java/pl/jdomanski/controller/DemoController.java
@@ -0,0 +1,33 @@
+package pl.jdomanski.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class DemoController {
+    
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+    
+    @GetMapping("welcome")
+    public String welcome(Model model){
+        model.addAttribute("user", "Jarek");
+        log.info("model = {}", model);
+        return "welcome";
+    }
+    
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage(){
+        log.info("welcomeMessage() called");
+        return "Welcome to demo app.";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome ${user}!</h1>
+    <h2>${welcomeMessage}</h2>
+</body>
+</html>
